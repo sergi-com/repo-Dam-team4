1010ef304e17354048533fef545fed3fb3b2ae89
Raul <raul.ferris.riera@cieep.com>
2024-05-02 13:27:18 +0200

    Modificacion de rutas

--- repositorio/src/app/app.component.ts
+++ repositorio/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { CardComponent } from './component/card/card.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { RegisterComponent } from './component/register/register.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,CardComponent,ContactComponent,FooterComponent,HomeComponent,RegisterComponent,SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
