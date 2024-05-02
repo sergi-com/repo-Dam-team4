700c0a0e93fb82332b2c59a4e3a21786f8bbe3a1
Raul <raul.ferris.riera@cieep.com>
2024-05-02 13:05:04 +0200

    Creación de rutas del primer merge

--- repositorio/src/app/app.routes.ts
+++ repositorio/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { CardComponent } from './component/card/card.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { RegisterComponent } from './component/register/register.component';
+import { SignInComponent } from './component/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'card', title: 'card', component: CardComponent},
+    {path: 'contact', title: 'contact', component: ContactComponent},
+    {path: 'footer', title: 'footer', component: FooterComponent},
+    {path: 'home', title: 'home', component: HomeComponent},
+    {path: 'navbar', title: 'navbar', component: NavbarComponent},
+    {path: 'register', title: 'register', component: RegisterComponent},
+    {path: 'sign-in', title: 'sign-in', component: SignInComponent}
+];
