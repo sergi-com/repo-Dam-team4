a19b0da2dc6689b8461d1e525bbfd10be2701420
Raul <raul.ferris.riera@cieep.com>
2024-05-03 10:37:50 +0200

    Creacion de home y creacion de ruta de home

---
+++ repositorio/src/app/component/pago/pago.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pago',
+  standalone: true,
+  imports: [],
+  templateUrl: './pago.component.html',
+  styleUrl: './pago.component.css'
+})
+export class PagoComponent {
+
+}
