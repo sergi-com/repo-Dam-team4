51ad374cbc7a77c3ddbd3f5958d8766f481b1a67
Felix <felix.apariciopeleguer@cieep.com>
2024-05-05 11:35:06 +0200

    Modificacion de Navbar y Footer y creacion de politica de privacidad

--- repositorio/src/app/app.routes.ts
+++ repositorio/src/app/app.routes.ts
@@ -9,6 +9,7 @@ import { SignInComponent } from './component/sign-in/sign-in.component';
 import { PagenotfoundComponent } from './component/pagenotfound/pagenotfound.component';
 import { AppComponent } from './app.component';
 import { PagoComponent } from './component/pago/pago.component';
+import { PrivacypolicyComponent } from './component/misc/privacypolicy/privacypolicy.component';
 
 export const routes: Routes = [
     {path: 'home', title: 'home', component: HomeComponent},
@@ -19,6 +20,7 @@ export const routes: Routes = [
     {path: 'register', title: 'register', component: RegisterComponent},
     {path: 'sign-in', title: 'sign-in', component: SignInComponent},
     {path: 'pago', title: 'pago', component: PagoComponent},
+    {path: 'privacypolicy', title:'privacypolicy', component:PrivacypolicyComponent },
     {path: "", redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: PagenotfoundComponent }
 
