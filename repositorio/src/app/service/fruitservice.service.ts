30c5456b2803a23f9e580a621a0163a2c7645ce2
Felix <felix.apariciopeleguer@cieep.com>
2024-05-16 17:17:39 +0200

    Correccion de API

--- repositorio/src/app/service/fruitservice.service.ts
+++ repositorio/src/app/service/fruitservice.service.ts
@@ -6,11 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FruitService {
-  private apiUrl = 'https://www.fruityvice.com/api/fruit/all'
+
 
   constructor(private http: HttpClient) { }
 
   getAllFruits(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>('./assets/data/api.json');
   }
 }
